7a501e62e404fd68e9ec971b66bc101d3cd2b5ae
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-09-29 23:45:13 +0800

    add cache file size.

--- cache_remover.cpp
+++ cache_remover.cpp
@@ -1,9 +1,35 @@
 #include "dir_size.h"
+#include <_types/_uint64_t.h>
+#include <cstddef>
 #include <filesystem>
 #include <iostream>
 #include <nlohmann/json.hpp>
 #include <nlohmann/json_fwd.hpp>
+#include <vector>
+inline std::vector<std::filesystem::path> get_cache_dirs() {
+  return {
+      ".cache",
+      "build",
+  };
+}
+inline uint64_t
+get_cache_dirs_size(std::filesystem::path root,
+                    std::vector<std::filesystem::path> cache_dirs) {
+  uint64_t size{};
+  for (auto sub : cache_dirs) {
+    auto dir = root / sub;
+    bool skip = false;
+    if (!std::filesystem::exists(dir)) {
+      skip = true;
+    }
 
+    if (skip) {
+      continue;
+    }
+    size += get_dir_size(dir);
+  }
+  return size;
+}
 int main(int argc, char **argv) {
   std::string json_str;
   while (true) {
@@ -18,6 +44,11 @@ int main(int argc, char **argv) {
     std::string p = repo["path"];
     auto repo_size = get_dir_size(p);
     repo["repo_size"] = repo_size;
+    auto cache_size = get_cache_dirs_size(p, get_cache_dirs());
+    repo["cache_size"] = cache_size;
+
+    auto code_size = repo_size - cache_size;
+    repo["code_size"] = code_size;
     std::cout << repo << "\n";
   }
 
