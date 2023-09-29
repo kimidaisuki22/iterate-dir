88a5612af25f831b3ac4c0c1461da7080fef68fe
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-09-29 12:43:30 +0800

    change output to json.

--- find-repo.cpp
+++ find-repo.cpp
@@ -1,12 +1,18 @@
 #include "include/dir-iter/dir-iter.h"
 #include <filesystem>
 #include <iostream>
+#include <nlohmann/json.hpp>
+#include <nlohmann/json_fwd.hpp>
 int main(int argc, char **argv) {
   auto root = argv[1];
-  dir_iter::simple_iterate(root, [](const std::filesystem::path &p) {
+  nlohmann::json json;
+  dir_iter::simple_iterate(root, [&json](const std::filesystem::path &p) {
     if (std::filesystem::is_directory(p) && p.filename() == ".git") {
-      std::cout << "path: " << p.parent_path() << "\n";
+      json["repo"].push_back(p.parent_path().string());
+      // std::cout << "path: " << p.parent_path() << "\n";
     }
   });
+
+  std::cout << json;
   return 0;
 }
