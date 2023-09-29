94141379bfac9ac922a64fc0130dbd5ae4ebce18
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-09-29 12:53:01 +0800

    get dir changed time with files in dir.

--- dir_time.h
+++ dir_time.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "include/dir-iter/dir-iter.h"
 #include <cassert>
 #include <chrono>
 #include <filesystem>
@@ -9,6 +10,10 @@ get_dir_changed_time(std::filesystem::path dir_path) {
   assert(std::filesystem::is_directory(dir_path));
 
   auto time = std::filesystem::last_write_time(dir_path);
+  dir_iter::simple_iterate(dir_path, [&time](std::filesystem::path p){
+    auto t = std::filesystem::last_write_time(p);
+    time = std::min(t,time);
+  });
   using Clock = decltype(time)::clock;
   auto now = Clock::now();
 
